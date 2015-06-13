bf58eeb2667b25c20169035dee7dff00ce485802
Joao Marcelo Barros <joaomarcelobarros@gmail.com>
2015-06-13 20:00:51 -0300

    Test WebPage

---
+++ ha_cluster/install_web.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+####created by jmbarros at tech4it.com.br                              ##### 
+####free to distributing, just give me some credits, okay ? :)         #####
+###########################################################################
+
+PORTS="22,80"
+
+#create directory
+mkdir $HOME/download_temp
+
+#get AFP and install AFP
+yum update -y
+yum install httpd -y
+wget -P $HOME/download_temp/ http://www.rfxnetworks.com/downloads/apf-current.tar.gz
+chmod +x $HOME/download_temp/apf-current.tar.gz
+tar -xvf $HOME/download_temp/apf-current.tar.gz -C $HOME/download_temp/
+cd $HOME/download_temp/apf-*
+/bin/bash install.sh
+
+#change the rules
+sed -i -e "s/DEVEL_MODE=.*/DEVEL_MODE=\"0\"/" /etc/apf/conf.apf
+sed -i -e "s/IFACE_IN=.*/IFACE_IN=\"eth1\"/" /etc/apf/conf.apf
+sed -i -e "s/IFACE_OUT=.*/IFACE_OUT=\"eth1\"/" /etc/apf/conf.apf
+sed -i -e "s/IFACE_TRUSTED=.*/IFACE_TRUSTED=\"eth0\"/" /etc/apf/conf.apf
+sed -i -e "s/IG_TCP_CPORTS=.*/IG_TCP_CPORTS=\"$PORTS\"/" /etc/apf/conf.apf
+
+
+echo "Server test - 50 Kb - " > /var/www/html/index2.php
+base64 /dev/urandom | head -c 50000 >> /var/www/html/index2.php
+
+#start
+/usr/local/sbin/apf -r
+/etc/init.d/httpd start
+
+#start service
+chkconfig httpd on
+chkconfig apf on
